c8b52c4297d287cc7f751dff5b61f0c0f5746b63
Pea <pea@misfist.com>
2021-07-21 14:58:12 -0400

    Add placeholder structure

---
+++ src/App/General/Queries.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * WP Action Network Events
+ *
+ * @package   WP_Action_Network_Events
+ */
+
+declare( strict_types = 1 );
+
+namespace WpActionNetworkEvents\App\General;
+
+use WpActionNetworkEvents\Common\Abstracts\Base;
+use WpActionNetworkEvents\App\General\PostTypes;
+
+/**
+ * Class Queries
+ *
+ * @package WpActionNetworkEvents\App\General
+ * @since 1.0.0
+ */
+class Queries extends Base {
+
+	/**
+	 * Initialize the class.
+	 *
+	 * @since 1.0.0
+	 */
+	public function init() {
+		/**
+		 * This general class is always being instantiated as requested in the Bootstrap class
+		 *
+		 * @see Bootstrap::__construct
+		 *
+		 * Add plugin code here
+		 */
+	}
+
+	/**
+	 * @param $posts_count
+	 * @param string $orderby
+	 * @return \WP_Query
+	 */
+	public function getPosts( $posts_count, $orderby = 'date' ): \WP_Query {
+		return new \WP_Query(
+			[
+				'post_type'      => PostTypes::POST_TYPE['id'],
+				'post_status'    => 'publish',
+				'order'          => 'DESC',
+				'posts_per_page' => $posts_count,
+				'orderby'        => $orderby,
+			]
+		);
+	}
+
+	/**
+	 * Example
+	 *
+	 * @return array
+	 */
+	public function getPostIds(): array {
+		global $wpdb;
+		return $wpdb->get_col( "select ID from {$wpdb->posts} LIMIT 3" );
+	}
+}
