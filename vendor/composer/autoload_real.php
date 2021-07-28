bf803eaea06daec7a334dfcb9edc161fab910143
Pea <pea@misfist.com>
2021-07-28 15:54:51 -0400

    Update class loaders

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInitb2d4d1c281fb806c8558ddb7295a87fe
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitb2d4d1c281fb806c8558ddb7295a87fe', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitb2d4d1c281fb806c8558ddb7295a87fe', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitb2d4d1c281fb806c8558ddb7295a87fe::getInitializer($loader));
         } else {
